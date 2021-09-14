7215366941e0f859eef2fb1dd37dc5eba3790a99
Shane Curtis <c84a@ymail.com>
2021-09-14 20:36:17 +0800

    Setup sequlize with express server.Created database model requirements

--- models/Category.js
+++ models/Category.js
@@ -2,12 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+
+    id: {
+
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    catergory_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
   },
+
   {
     sequelize,
     timestamps: false,
